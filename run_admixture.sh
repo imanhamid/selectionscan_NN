04aeca61686c68d94cbdace13503de04142bb2b2
Iman Hamid <imanhamid95@gmail.com>
2021-04-13 19:11:38 -0400

    general slimulation

---
+++ run_admixture.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p scavenger
+#SBATCH --job-name="SLiMulation"
+#SBATCH -a 1-10
+
+c=$SLURM_ARRAY_TASK_ID
+
+declare -i c100="${c}00"
+
+for i in {0..99};
+do
+
+c_array[$i]=$((c100 - i))
+
+done
+
+for i in "${c_array[@]}"
+do
+
+/hpc/home/ih49/home/SLiM_build/slim -d L=50000000 -d N=10000 \
+-d mu=-0.01314833 -d alpha=0.186 -d mig=0.5 -d t_end=50 \
+-d out='"/work/ih49/simulations/test_NN/human_L-50_N-10000_single-pulse_m-0.5"' \
+-d seed=\'"seed-$i"\' ~/home/selectionscan_NN/admixture.slim
+
+done
+
