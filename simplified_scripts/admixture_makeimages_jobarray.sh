9def26c21aa82098470b6463616c008f68bca2e5
Iman Hamid <imanhamid95@gmail.com>
2021-09-02 21:18:58 -0400

    add notebook for runnign icevision on cluster

---
+++ simplified_scripts/admixture_makeimages_jobarray.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -p scavenger
+#SBATCH --job-name="png"
+#SBATCH -a 1-100
+#SBATCH --mem=5G
+#SBATCH -o "100_png.out"
+
+module load R/4.0.0
+
+c=$SLURM_ARRAY_TASK_ID
+
+declare -i c100="${c}00"
+
+for i in {0..99};
+do
+
+c_array[$i]=$((c100 - i))
+
+done
+
+for i in "${c_array[@]}"
+do
+
+
+filename=$(ls /work/ih49/simulations/AIMs/*_seed-${i}_alltracts.txt | head -n 1)
+~/home/selectionscan_NN/simplified_scripts/admixture_makeimage.R $filename 50000000 200 /hpc/home/ih49/home/selectionscan_NN/100_AIM_positions_L-50Mb.RData
+
+done
